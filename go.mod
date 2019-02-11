31748ffc7d2a1c27bea2b9a4d74a00701243fdc6
Vuraddo <vuraddo@yahoo.com>
2019-02-11 13:23:48 +0700

    Add database connection

--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module google-app-engine-sample
 
 require (
+	github.com/go-pg/pg v7.1.7+incompatible
 	github.com/go-redis/redis v6.15.1+incompatible
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/kelseyhightower/envconfig v1.3.0
 	github.com/nats-io/go-nats v1.7.0
 	github.com/nats-io/nkeys v0.0.2 // indirect
 	github.com/nats-io/nuid v1.0.0 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
 	"github.com/go-redis/redis"
 	"github.com/kelseyhightower/envconfig"
 )
@@ -13,6 +16,18 @@ import (
 type EnvConf struct {
 	RedisHost string `envconfig:"REDIS_HOST"`
 	RedisPort uint64 `envconfig:"REDIS_PORT"`
+	DbHost string `envconfig:"DB_HOST"`
+	DbPort uint64 `envconfig:"DB_PORT"`
+	DbName string `envconfig:"DB_NAME"`
+	DbUser string `envconfig:"DB_USER"`
+	DbPassword string `envconfig:"DB_PASSWORD"`
+}
+
+type User struct {
+	Id uint `sql:",type:bigserial"`
+	CreatedAt time.Time `sql:",notnull,default:CURRENT_TIMESTAMP"`
+
+	Name string `sql:",notnull,type:varchar(120)"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -20,17 +35,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	envConf := new(EnvConf)
-	err := envconfig.Process("", envConf)
+	conf := new(EnvConf)
+	err := envconfig.Process("", conf)
+	// TODO: Use validator
 	if err != nil {
 		log.Fatalln("parse env configs,", err)
-	} else if envConf.RedisHost == "" {
+	} else if conf.RedisHost == "" {
 		log.Fatalln("REDIS_HOST is not defined")
-	} else if envConf.RedisPort == 0 {
-		log.Fatalln("REDIS_PORT is not defined")
+	} else if conf.DbHost == "" {
+		log.Fatalln("DB_HOST is not defined")
+	} else if conf.DbName == "" {
+		log.Fatalln("DB_NAME is not defined")
+	} else if conf.DbUser == "" {
+		log.Fatalln("DB_USER is not defined")
+	} else if conf.DbPassword == "" {
+		log.Fatalln("DB_PASSWORD is not defined")
 	}
 
-	redisAddr := envConf.RedisHost + ":" + strconv.FormatUint(envConf.RedisPort, 10)
+	if conf.RedisPort == 0 {
+		conf.RedisPort = 6379
+	}
+	if conf.DbPort == 0 {
+		conf.DbPort = 5432
+	}
+
+	// Connect to Memorystore Redis instance
+	redisAddr := conf.RedisHost + ":" + strconv.FormatUint(conf.RedisPort, 10)
 	log.Println("connect to redis on", redisAddr)
 	client := redis.NewClient(&redis.Options{
 		Addr:     redisAddr,
@@ -41,6 +71,26 @@ func main() {
 		log.Fatalln("ping failed,", err)
 	} else if pingResp != "PONG" {
 		log.Fatalln("ping failed")
+	}
+
+	// Connect to database
+	dbAddr := conf.DbHost + ":" + strconv.FormatUint(conf.DbPort, 10)
+	log.Println("connect to database on", dbAddr)
+	db := pg.Connect(&pg.Options{
+		Addr:     dbAddr,
+		User:     conf.DbUser,
+		Database: conf.DbName,
+		Password: conf.DbPassword,
+	})
+
+	// Create users table if not exists
+	createOptions := orm.CreateTableOptions{
+		FKConstraints: true,
+		IfNotExists:   true,
+	}
+	err = db.CreateTable(&User{}, &createOptions)
+	if err != nil {
+		log.Println("create users table,", err)
 	}
 
 	http.HandleFunc("/", handler)
